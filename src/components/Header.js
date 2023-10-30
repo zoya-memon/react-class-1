bd1d7f691fa81f4ed8bea2534ad41478b87e1b2f
zoya-memon <zoya.bcsf18@iba-suk.edu.pk>
2023-10-30 15:29:02 +0500

    first-commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// there are 3 ways to create a react copmponent
+// method 1: default functional component
+// method 2: arrow functional component
+// method 3: default return functional component
+
+// method 1:
+export default function Header() {
+    return(
+        <>
+            <h1>Header</h1>
+        </>
+    )
+}
---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+// there are 3 ways to create a component
+// method 1: default export
+// method 2: arrow function
+// method 3: default return
+
+// method 2:
+export const HeroSection = () => {
+    return(
+        <>
+            <h1>Hero Section</h1>
+        </>
+    )
+}
